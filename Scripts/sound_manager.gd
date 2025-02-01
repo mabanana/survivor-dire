29956e2025fe4a2b136a9ded3cfaffa9542791ef
mabanana <aldwin.yu@hotmail.com>
2025-02-01 18:18:26 -0500

    Update Audio processing

--- Scripts/sound_manager.gd
+++ Scripts/sound_manager.gd
@@ -5,14 +5,19 @@ var core_changed: Signal
 var main_scene: Main
 var sounds: Dictionary
 var sounds_dir = "res://Resources/Sounds/"
-var audio_stream_player: AudioStreamPlayer
+var master_player: AudioStreamPlayer
+var bgm_player: AudioStreamPlayer
+var notif_player: AudioStreamPlayer
+var hit_fx_player: AudioStreamPlayer
 
 func _init(main_scene: Main):
 	self.main_scene = main_scene
-	audio_stream_player = AudioStreamPlayer.new()
-	audio_stream_player.set_max_polyphony(10)
-	main_scene.add_child(audio_stream_player)
+	init_audio_stream_players()
+	
 	sounds = {}
+	_init_sounds_dict()
+
+func _init_sounds_dict():
 	var folder_dir_access = DirAccess.open(sounds_dir)
 	for file_name in folder_dir_access.get_files():
 		#print("sound file found: ", file_name)
@@ -21,6 +26,20 @@ func _init(main_scene: Main):
 		var file_type = split_file_name[len(split_file_name) - 1]
 		if file_type in ["wav", "ogg", "mp3"]:
 			sounds[sound_name] = sounds_dir + "/" + file_name
+
+func init_audio_stream_players():
+	master_player = AudioStreamPlayer.new()
+	bgm_player = AudioStreamPlayer.new()
+	notif_player = AudioStreamPlayer.new()
+	hit_fx_player = AudioStreamPlayer.new()
+	
+	hit_fx_player.set_max_polyphony(10)
+	
+	main_scene.add_child(master_player)
+	main_scene.add_child(bgm_player)
+	main_scene.add_child(notif_player)
+	main_scene.add_child(hit_fx_player)
+
 
 func bind(core: CoreModel, core_changed):
 	self.core = core
@@ -32,29 +51,40 @@ func _on_core_changed(context, payload):
 	if context == core.Context.damage_ended:
 		match payload[core.PKey.attack_type]:
 			"auto":
-				_spawn_audio_player("auto_attack", -10, "Master", Vector2(1.8,1.9))
+				_play_audio("auto_attack", -10, "HitFx", Vector2(1.8,1.9))
 			"aoe":
-				_spawn_audio_player("aoe_attack", -20, "Master", Vector2(0.8,0.9))
+				_play_audio("aoe_attack", -20, "HitFx", Vector2(0.8,0.9))
 			"click":
-				_spawn_audio_player("click_attack", -18, "Master", Vector2(0.8,0.9))
+				_play_audio("click_attack", -18, "HitFx", Vector2(0.8,0.9))
 			_:
-				_spawn_audio_player("auto_attack", -10, "Master", Vector2(1.8,1.9))
+				_play_audio("auto_attack", -10, "HitFx", Vector2(1.8,1.9))
 	elif context == core.Context.loot_dropped:
 		match payload[core.PKey.loot_type]:
 			"Gold":
-				_spawn_audio_player("gold_pickup", 0, "Notification", Vector2(0.8,0.9))
+				_play_audio("gold_pickup", 0, "Notification", Vector2(0.8,0.9))
 			"HP":
-				_spawn_audio_player("hp_pickup", 0, "Notification", Vector2(0.8,0.9))
+				_play_audio("hp_pickup", 0, "Notification", Vector2(0.8,0.9))
 			"XP":
-				_spawn_audio_player("xp_pickup", 0, "Notification", Vector2(0.8,0.9))
+				_play_audio("xp_pickup", 0, "Notification", Vector2(0.8,0.9))
 			"Artefact":
-				_spawn_audio_player("artefact_pickup", 0, "Notification", Vector2(0.8,0.9))
+				_play_audio("artefact_pickup", 0, "Notification", Vector2(0.8,0.9))
 			_:
 				pass
 		
 # TODO: use finite number of audio_players instead of instantiating new ones
-func _spawn_audio_player(sound_name: String, db_offset: float, bus = "Master", pitch_range: Vector2 = Vector2(0.95, 1.05), position: Vector2 = Vector2.ZERO):
-	var audio_player = audio_stream_player
+func _play_audio(sound_name: String, db_offset: float, bus = "Master", pitch_range: Vector2 = Vector2(0.95, 1.05), position: Vector2 = Vector2.ZERO):
+	var audio_player
+	match bus:
+		"Master":
+			audio_player = master_player
+		"BGM":
+			audio_player = bgm_player
+		"Notification":
+			audio_player = notif_player
+		"HitFx":
+			audio_player = hit_fx_player
+		_:
+			audio_player = master_player
 	audio_player.volume_db = db_offset
 	audio_player.stream = load(sounds[sound_name])
 	audio_player.pitch_scale = randf_range(pitch_range.x, pitch_range.y)
