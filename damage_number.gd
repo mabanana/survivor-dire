47238056c7818b13f259216a65220e3070ce18ec
mabanana <aldwin.yu@hotmail.com>
2025-02-01 21:39:37 -0500

    edit damage number

--- damage_number.gd
+++ damage_number.gd
@@ -3,7 +3,7 @@ class_name DamageNumber
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var new_pos = position + Vector2.UP * 20
+	var new_pos = position + Vector2.UP.rotated(deg_to_rad(randf_range(-20,20))) * randf_range(20,70)
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(self, "position", new_pos, 0.6)
 	tween.set_ease(Tween.EASE_OUT)
